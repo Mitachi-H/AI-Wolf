4cff13f751b5fb285f0436c3e3a4c46ae5ae20c2
Hiroya M <hiroya.m33@icloud.com>
2024-03-18 17:18:07 +0900

    プロンプト改善

--- lib/LLM/chatgpt_agent.py
+++ lib/LLM/chatgpt_agent.py
@@ -8,6 +8,8 @@ class ChatGPTAgent(OpenAIClient):
         self.agent_idx = None
         self.role = None
         self.prompts_loaded = False
+
+        self.discussionHistory = {"talk": [], "vote": [], "divine": [], "attack": []}
     
     def set_agent_idx(self, agent_idx: int) -> None:
         self.agent_idx = agent_idx
@@ -36,26 +38,22 @@ class ChatGPTAgent(OpenAIClient):
             self.load_prompts()
 
         system = f"""
-あなたの名前はAgent[0{self.agent_idx}]です。役職{self.role}としてプレイ中の人狼ゲームでの発言を考えなさい。ただし出力ルールを守らないと罰せられます。
-# 出力ルール
+あなたの名前はAgent[0{self.agent_idx}]です。
 {self.talk_output_rule}
 """     
         messages = []
-        messages.append("## 人狼ゲームのルール\n"+self.game_ex)
         messages.append("## 役職の詳細\n"+self.role_ex)
         messages.append("## あなたの役職\n"+self.role)
-        messages.append("## あなたの戦術\n"+self.talk_tactics)
-        messages.append("### (参考）発言例\n"+self.statements)  
         messages.append("## ゲーム記録"+"\n".join(gameTextRecords))
+        messages.append("## 過去の自己議論"+"\n".join(self.discussionHistory["talk"]))
         messages.append(f"# システム文（再掲） {system}")
-
-        # print(f"system: {system}\n messages: {messages}")
 
         talk_text = self.chat(system, messages)
         print(f"Agent[0{self.agent_idx}]({self.role})'s talk_text")
         print(talk_text)
         print("\n")
         talk = json.loads(talk_text)["発言"]
+        self.discussionHistory["talk"].append(talk["議論"])
         return talk
 
 
@@ -64,18 +62,14 @@ class ChatGPTAgent(OpenAIClient):
             self.load_prompts()
 
         system = f"""
-あなたの名前はAgent[0{self.agent_idx}]です。役職{self.role}としてプレイ中の人狼ゲームで投票先を考えなさい。ただし出力ルールを守らないと罰せられます。
-# 出力ルール
+あなたの名前はAgent[0{self.agent_idx}]です。
 {self.vote_output_rule}
 """     
         messages = []
-        messages.append(f"## 人狼ゲームのルール\n{self.game_ex}")
         messages.append(f"## 役職の詳細\n{self.role_ex}")
         messages.append(f"## あなたの役職\n{self.role}")
-        messages.append(f"## あなたの議論戦術\n{self.talk_tactics}")
-        messages.append(f"## あなたの投票戦術\n{self.vote_tactics}")
         messages.append("## ゲーム記録"+"\n".join(gameTextRecords))
-
+        messages.append("## 過去の自己議論"+"\n".join(self.discussionHistory["talk"]))
         messages.append(f"# システム文（再掲） {system}")
 
         vote_text = self.chat(system, messages)
@@ -83,6 +77,7 @@ class ChatGPTAgent(OpenAIClient):
         print(vote_text)
         print("\n")
         vote = json.loads(vote_text)["結論"]
+        self.discussionHistory["vote"].append(vote["議論"])
         return json.dumps(vote,separators=(",",":"))
 
 
@@ -91,24 +86,22 @@ class ChatGPTAgent(OpenAIClient):
             self.load_prompts()
 
         system = f"""
-あなたの名前はAgent[0{self.agent_idx}]です。役職{self.role}としてプレイ中の人狼ゲームで占い先を考えなさい。ただし出力ルールを守らないと罰せられます。
-# 出力ルール
+あなたの名前はAgent[0{self.agent_idx}]です。
 {self.divine_output_rule}
 """     
         messages = []
-        messages.append(f"## 人狼ゲームのルール\n{self.game_ex}")
         messages.append(f"## 役職の詳細\n{self.role_ex}")
         messages.append(f"## あなたの役職\n{self.role}")
-        messages.append(f"## あなたの議論戦術\n{self.talk_tactics}")
-        messages.append(f"## あなたの占い戦術\n{self.divine_tactics}")
         messages.append("## ゲーム記録"+"\n".join(gameTextRecords))
-        messages.append(f"# システム文（再掲） {system}")
+        messages.append("## 過去の自己議論"+"\n".join(self.discussionHistory["talk"]))
+        messages.append(f"# 命令文（再掲）\n{system}")
 
         divine_text = self.chat(system, messages)
         print(f"Agent[0{self.agent_idx}]({self.role})'s divine_text")
         print(divine_text)
         print("\n")
         divine = json.loads(divine_text)["結論"]
+        self.discussionHistory["divine"].append(divine["議論"])
         return json.dumps(divine,separators=(",",":"))
 
 
@@ -117,24 +110,22 @@ class ChatGPTAgent(OpenAIClient):
             self.load_prompts()
 
         system = f"""
-あなたの名前はAgent[0{self.agent_idx}]です。役職{self.role}としてプレイ中の人狼ゲームで襲撃先を考えなさい。ただし出力ルールを守らないと罰せられます。
-# 出力ルール
+あなたの名前はAgent[0{self.agent_idx}]です
 {self.attack_output_rule}
 """     
         messages = []
-        messages.append(f"## 人狼ゲームのルール\n{self.game_ex}")
         messages.append(f"## 役職の詳細\n{self.role_ex}")
         messages.append(f"## あなたの役職\n{self.role}")
-        messages.append(f"## あなたの議論戦術\n{self.talk_tactics}")
-        messages.append(f"## あなたの襲撃戦術\n{self.attack_tactics}")
         messages.append("## ゲーム記録"+"\n".join(gameTextRecords))
-        messages.append(f"# システム文（再掲） {system}")
+        messages.append("## 過去の自己議論"+"\n".join(self.discussionHistory["talk"]))
+        messages.append(f"# 命令文（再掲）\n{system}")
 
         attack_text = self.chat(system, messages)
         print(f"Agent[0{self.agent_idx}]({self.role})'s attack_text")
         print(attack_text)
         print("\n")
         attack = json.loads(attack_text)["結論"]
+        self.discussionHistory["attack"].append(attack["議論"])
         return json.dumps(attack,separators=(",",":"))
 
 if __name__ == "__main__":
